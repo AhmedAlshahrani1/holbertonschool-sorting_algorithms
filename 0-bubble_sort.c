075287c6f6f2c4e8d5e22451cb6ef476d4bd5421
AhmedAlshahrani1 <ahmed.f.alhatim@gmail.com>
2026-05-07 16:58:45 +0300

    Task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,11 +15,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
-			if (array [j] > array [j + 1])
+			if (array[j] > array[j + 1])
 			{
-				temp = array [j];
-				array [j] = array [j + 1];
-				array [j + 1] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
